19c710829aac94a7af596908af35798176a0b841
Sandro Peixoto <sandro.peixoto@gmail.com>
2025-10-29 19:52:03 -0300

    Increase page size from 25 to 100

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ class LexmarkCFMClient:
         """
         all_assets: List[Dict[str, Any]] = []
         page = 0
-        page_size = 25
+        page_size = 100
 
         try:
             with st.spinner("Buscando todas as impressoras (paginação)..."):
